02096d491a3b8657f49885346c8fac38b9ae7ada
Dylan Freeman <35830457+templarmanatee@users.noreply.github.com>
2022-12-28 21:17:42 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+const inquirer = require('Inquirer'); 
+// TODO: Create an array of questions for user input
+const questions = [
+    'Project Title: ',
+    'Description: ',
+    'Installation Instructions: ', 
+    'Usage Guidelines: ', 
+    'Contributors: ',
+    'Tests: '
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    
+}
+
+// Function call to initialize app
+init();
