8d6a497af2c92a8821a7e171af2251edc5a1e2a3
TemplarManatee <dylanbrad10@gmail.com>
2023-02-12 23:32:39 -0500

    Pretty much done

--- index.js
+++ index.js
@@ -1,23 +1,72 @@
 // TODO: Include packages needed for this application
-const inquirer = require('Inquirer'); 
+const inquirer = require('inquirer'); 
+const fs = require('fs'); 
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
+
 const questions = [
-    'Project Title: ',
-    'Description: ',
-    'Installation Instructions: ', 
-    'Usage Guidelines: ', 
-    'Contributors: ',
-    'Tests: '
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the project title: ',
+    },    
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter the project description: ',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter the installation guidelines: ',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter the usage instructions: ',
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Who else needs to be credited?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What is your preferred license?',
+        choices: ['Apache', 'MIT', 'GPL'],
+    },    
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What tests were run, if any?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:' 
+    }, 
+    {
+        type: 'input', 
+        name: 'email',
+        message: 'What is your e-mail?'
+    }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+    fs.writeFile(fileName, data, function (err) {
+        if(err){
+            return console.log(err);
+        }
+    })
 }
 
 // TODO: Create a function to initialize app
-function init() {
-    
+async function init() {
+    const response = await inquirer.prompt(questions);
+    const formatted_response = generateMarkdown(response); 
+    await writeToFile(`readmes/${response.title}`, formatted_response);
 }
 
 // Function call to initialize app
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,73 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  return `![badge](https://shields.io/badge/license-${license}-green)`;
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  switch(license) {
+    case 'Apache': 
+      return 'https://www.apache.org/licenses/LICENSE-2.0';
+    case 'MIT': 
+      return '';
+    case 'GPL': 
+      return 'https://www.gnu.org/licenses/gpl-3.0.en.html'; 
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  const link = renderLicenseLink(license); 
+  if(!link)
+    return `This project is covered under the ${license} license.`; 
+  else
+    return `This project is covered under the ${license} license.
+
+  ${link}`; 
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+${renderLicenseBadge(data.license)}  
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation 
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## Credits 
+
+${data.contributors}
+
+## License 
+
+${renderLicenseSection(data.license)}
+
+## Contact Me
+
+![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white) [${data.github}](https://github.com/${data.github}/)
+
+You can reach me at ${data.email} with any questions! 
+`;
+}
+
+module.exports = generateMarkdown;
